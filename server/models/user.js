94c156ddbe3ea3121c0572a959f8f6d73900bc2c
papapin <papapin777#gmail.com>
2019-05-02 20:38:09 -0700

    SERVER: Hashing passwords

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const SALT_I = 10;
 
 const userSchema = mongoose.Schema({
         email:{
@@ -40,6 +42,26 @@ const userSchema = mongoose.Schema({
 
 
 });
+
+userSchema.pre('save', function(next) {
+    let user = this;
+    
+    if( user.isModified('password') ) {
+        bcrypt.genSalt(SALT_I, function (err, salt) {
+            if (err) return next(err);
+
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err);
+                user.password = hash;
+                next();
+            })
+        })
+    } else {
+        next();
+    }
+})
+
+
 
 const User = mongoose.model('User', userSchema);
 
